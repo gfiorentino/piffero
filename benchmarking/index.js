1738f06032c28c4c796d0afd74969e4a40b28081
Nigro Simone <nigro.simone@gmail.com>
2021-03-13 22:38:47 +0100

    fix test

--- benchmarking/index.js
+++ benchmarking/index.js
@@ -16,37 +16,6 @@ var Piffero = require('../dist/index.js').Piffero;
 var JSON_FILE = '../spec/jsonFiles/large.json';
 var JSON_PATH = '[2].tags[2]';
 
-
-// run test ...
-
-suite.add('oboe', {
-    defer: true,
-    fn: function (deferred) {
-      
-        oboe(fs.createReadStream(JSON_FILE)).node(JSON_PATH, function (result) {
-            this.abort();
-            deferred.resolve();
-        });
-
-    }
-})
-.add('Piffero', {
-    defer: true,
-    fn: function (deferred) {
-      
-        Piffero.findAsString(function (result) {
-                deferred.resolve();
-            },  
-            fs.createReadStream(JSON_FILE), 
-            '$' + JSON_PATH
-        );
-
-var testMethods = [oboeTest, pifferoTest];
-
-testMethods.forEach(function(method) {
-    method();
-});
-
 // run test ...
 
 suite.add('oboe', {
