46ea072739cac0e5373ac9bb688ce570facac09e
giovanni fiorentino <gtinto@gmail.com>
2021-03-13 21:54:49 +0100

    update (piffero is faster)

--- benchmarking/index.js
+++ benchmarking/index.js
@@ -5,9 +5,6 @@
 var fs = require("fs");
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite;
-
 
 // JsonPath streaming libs
 var oboe = require('./oboe-node.js');
@@ -17,36 +14,6 @@ var Piffero = require('../dist/index.js').Piffero;
 var JSON_FILE = '../spec/jsonFiles/large.json';
 var JSON_PATH = '[2].tags[2]';
 
-
-// run test ...
-
-suite.add('oboe', {
-    defer: true,
-    fn: function (deferred) {
-      
-        oboe(fs.createReadStream(JSON_FILE)).node(JSON_PATH, function (result) {
-            this.abort();
-            deferred.resolve();
-        });
-
-    }
-})
-.add('Piffero', {
-    defer: true,
-    fn: function (deferred) {
-      
-        Piffero.findAsString(function (result) {
-                deferred.resolve();
-            },  
-            fs.createReadStream(JSON_FILE), 
-            '$' + JSON_PATH
-        );
-
-var testMethods = [oboeTest, pifferoTest];
-
-testMethods.forEach(function(method) {
-    method();
-});
 
 // run test ...
 
