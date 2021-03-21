7492bdff354d3c563027e4c30385da792532db42
giovanni fiorentino <gtinto@gmail.com>
2021-03-21 21:08:03 +0100

    1.1.4

--- src/handler/mastehandler.ts
+++ src/handler/mastehandler.ts
@@ -22,7 +22,7 @@ export class MasterHandler {
     isBulk: boolean = false
   ): Stream {
     this.callback = callback;
-    this.stream = stream as Readable ;
+    this.stream = stream as Readable;
     this.useString = useString;
     this.output = new Stream.Transform();
 
@@ -80,8 +80,7 @@ export class MasterHandler {
     this.currentHandler = this.stepHandlers[this.handlerIndex];
 
     this.shiftParser();
-    if(this.stream.pipe)
-    this.stream.pipe(this.cStream);
+    if (this.stream.pipe) this.stream.pipe(this.cStream);
     this.cStream = new CStream(this);
 
     if (!useString) {
