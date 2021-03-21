1a33cb26003d5d9098251b441929f5f1e5d8025a
giovanni fiorentino <gtinto@gmail.com>
2021-03-21 21:51:02 +0100

    update

--- bw.sh
+++ bw.sh
@@ -1,47 +1,51 @@
 #!/bin/sh
-cp dist/index.js dist/browser.js 
+cp dist/index.js dist/b.js 
+mkdir dist/src2
+cp -a dist/src/* dist/src2/
+
+browserify dist/b.js -o dist/browser.js
 
 src=browser 
 
-for entry in dist/src/handler/*
+for entry in dist/src2/handler/*
 do
   if [[ $entry == *js ]]; 
   then
   echo $entry  
-  entry2="${entry/src/$src}" 
+  entry2="${entry/src2/$src}" 
   browserify $entry -o $entry2 
   fi
 done
 
-for entry in dist/src/libs/clarinet/*
+for entry in dist/src2/libs/clarinet/*
 do
   if [[ $entry == *js ]]; 
   then
   echo $entry  
-  entry2="${entry/src/$src}" 
+  entry2="${entry/src2/$src}" 
   browserify $entry -o $entry2 
   fi
 done
 
-for entry in dist/src/conditioneval/*
+for entry in dist/src2/conditioneval/*
 do
   if [[ $entry == *js ]]; 
   then
   echo $entry  
-  entry2="${entry/src/$src}" 
+  entry2="${entry/src2/$src}" 
   browserify $entry -o $entry2 
   fi
 done
 
-for entry in dist/src/*
+for entry in dist/src2/*
 do
   if [[ $entry == *js ]]; 
   then
   echo $entry  
-  entry2="${entry/src/$src}" 
+  entry2="${entry/src2/$src}" 
   browserify $entry -o $entry2 
   fi
 done
 
-browserify dist/browser.js -o dist/browser.js,
 
+rm -rf dist/src2;
