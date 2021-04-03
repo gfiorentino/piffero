241dc02774ee6b28fe3c001f439c2c6c12052d01
giovanni fiorentino <gtinto@gmail.com>
2021-04-03 14:09:36 +0200

    update test

--- spec/piffero/errorSpec.ts
+++ spec/piffero/errorSpec.ts
@@ -4,14 +4,21 @@ import { PifferoError } from "../../src/pifferoerror";
 
 describe("piffero Error", function() {
     const fs  =  require('fs');
-    let stream
-    beforeEach( function() {   
-      stream = fs.createReadStream('spec/jsonFiles/error-two-dots.json');
-    });
+
 
     it("error two dots", async function(done) {
+        const stream = fs.createReadStream('spec/jsonFiles/error-two-dots.json');
         const callback = (json, error) => {
             expect(error).toEqual(new PifferoError( "Invalid number has two dots\nLine: 3\nColumn: 16\nChar: 46"));
+            done();
+          };
+        const result  = Piffero.findAsString(callback, stream ,'$.error.number');
+    });
+
+    it("error", async function(done) {
+        const stream = fs.createReadStream('spec/jsonFiles/error.json');
+        const callback = (json, error) => {
+            expect(error).toEqual(new PifferoError( "Unexpected end\nLine: 5\nColumn: 1\nChar: NaN"));
             done();
           };
         const result  = Piffero.findAsString(callback, stream ,'$.error.number');
